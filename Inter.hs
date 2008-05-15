283cce606462caa5041ae0e5611759544b9d4ede
bjorn <bjorn@bringert.net>
2008-05-15 12:39:17 +0100

    Added still unused continuation-based interpretation functor.
    darcs-hash:20080515113917-6cdb2-fce8d91dda85e179ae000c08ada0fed45db1cb42

---
+++ Inter.hs
@@ -0,0 +1,32 @@
+module Inter (I, cont, retrieve, retrieveFun) where
+
+import FOL
+
+import Control.Monad.Cont
+
+import Control.Applicative
+import Data.List
+import Data.Monoid
+
+newtype I a = I [Cont Prop a]
+
+instance Functor I where
+    fmap f x = pure f <*> x
+
+-- I is not a monad, since we would need to be able to use the 
+-- arguments of bind in any order.
+instance Applicative I where
+    pure a         = I [Cont ($ a)]
+    I xs <*> I ys  = I $ [Cont $ \c -> runCont x $ \f -> runCont y $ \a -> (c (f a))
+                           | x <- xs, y <- ys] ++
+                         [Cont $ \c -> runCont y $ \a -> runCont x $ \f -> (c (f a))
+                           | x <- xs, y <- ys]
+
+cont :: ((a -> Prop) -> Prop) -> I a
+cont f = I [Cont f]
+
+retrieve :: I Prop -> [Prop]
+retrieve (I xs) = map (\x -> runCont x id) xs
+
+retrieveFun :: I (Exp -> Prop) -> [Exp -> Prop]
+retrieveFun (I xs) = [\e -> runCont x ($ e) | x <- xs]
---
+++ InterExample.hs
@@ -0,0 +1,146 @@
+import FOL
+
+import Inter
+
+import Control.Monad.Cont
+
+import Control.Applicative
+import Data.List
+import Data.Monoid
+
+import Text.PrettyPrint.HughesPJ hiding (char)
+
+
+--
+-- Example abstract syntax
+--
+
+data Utt = DeclCl Cl | QuestCl Cl | QuestVP IP VP
+  deriving Show
+
+data IP = Who
+  deriving Show
+
+data Cl = PredVP NP VP
+  deriving Show
+
+data VP = ComplV2 V2 NP
+        | UseV V
+  deriving Show
+
+data NP = DetCN Det CN
+        | Everyone
+        | Someone
+        | John
+  deriving Show
+
+data Det = Every | A
+  deriving Show
+
+data CN = Man | Woman
+  deriving Show
+
+data V = Sleep
+  deriving Show
+
+data V2 = Love
+  deriving Show
+
+--
+-- Example semantics
+--
+
+data Input = Statement Prop | YNQ Prop | WhQ (Exp -> Prop)
+
+instance Eq Input where
+    x == y = show x == show y
+
+instance Ord Input where
+    compare x y = compare (show x) (show y)
+
+instance Show Input where
+    showsPrec n = showString . render . runVars . pprInput n
+
+pprInput :: Int -> Input -> Vars Doc
+pprInput n (Statement p) = wrapProp "stm" p
+pprInput n (YNQ p) = wrapProp "ynq" p
+pprInput n (WhQ u) = wrapFun "whq" u
+
+wrapProp :: String -> Prop -> Vars Doc
+wrapProp s p = liftM ((text s <>) . parens) (pprProp 0 p)
+
+wrapFun :: String -> (Exp -> Prop) -> Vars Doc
+wrapFun o u = do v <- getUnique
+                 p <- pprProp 0 (u (Var v))
+                 return $ text o <> parens (text v <> text "," <> p)
+
+
+
+iUtt :: Utt -> [Input]
+iUtt (DeclCl cl) = nub $ map Statement $ retrieve $ iCl cl
+iUtt (QuestCl cl) = nub $ map YNQ $ retrieve $ iCl cl
+iUtt (QuestVP ip vp) = nub $ map WhQ $ retrieveFun $ iIP ip <*> iVP vp
+
+iIP :: IP -> I ((Exp -> Prop) -> (Exp -> Prop))
+iIP Who = pure id
+
+iCl :: Cl -> I Prop
+iCl (PredVP np vp) = iNP np <*> iVP vp
+
+iVP :: VP -> I (Exp -> Prop)
+iVP (ComplV2 v2 np) = iV2 v2 <*> iNP np
+iVP (UseV v) = iV v
+
+iNP :: NP -> I ((Exp -> Prop) -> Prop)
+iNP (DetCN det cn) = iDet det <*> iCN cn
+iNP Everyone = cont $ \c -> forAll  (\x -> c ($ x))
+iNP Someone  = cont $ \c -> thereIs (\x -> c ($ x))
+iNP John = pure ($ FOL.Const "john")
+
+iDet :: Det -> I ((Exp -> Prop) -> (Exp -> Prop) -> Prop)
+iDet Every = cont $ \c -> forAll (\x -> c (\u v -> u x ==> v x))
+iDet A = cont $ \c -> thereIs (\x -> c (\u v -> u x ==> v x))
+
+iCN :: CN -> I (Exp -> Prop)
+iCN Man   = pure (\x -> Pred "man"   [x])
+iCN Woman = pure (\x -> Pred "woman" [x])
+
+iV :: V -> I (Exp -> Prop)
+iV Sleep = pure (\x -> Pred "sleep" [x])
+
+iV2 :: V2 -> I (((Exp -> Prop) -> Prop) -> Exp -> Prop)
+iV2 Love = pure (\u x -> u (\y -> Pred "love" [x,y]))
+
+
+
+
+
+--
+-- Old stuff
+--
+
+
+{-
+data Cont r a = Cont ((a -> r) -> r)
+                 | forall b. Apply (Cont r (b -> a)) (Cont r b)
+
+instance Functor (Cont r) where
+    fmap f x = pure f <*> x
+
+instance Applicative (Cont r) where
+    pure a   = Cont ($ a)
+    x <*> y  = Apply x y
+-}
+{-
+newtype Cont r a = Cont { runCont :: (a -> [r]) -> [r] }
+
+instance Functor (Cont r) where
+    fmap f x = pure f <*> x
+
+-- Cont is not a monad, since we would need to be able to use the 
+-- arguments of bind in any order.
+instance Applicative (Cont r) where
+    pure a   = Cont ($ a)
+    x <*> y  = Cont $ \c ->    (runCont x $ \f -> runCont y $ \a -> (c (f a)))
+                            ++ (runCont y $ \a -> runCont x $ \f -> (c (f a)))
+-}
